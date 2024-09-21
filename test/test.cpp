68e56311f148cccba08f9db8da7463212d77b194
Juuso Särkelä <71127114+juusosar@users.noreply.github.com>
2024-09-21 13:09:43 +0300

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -11,21 +11,12 @@ FUZZ_TEST_SETUP() {
 }
 
 FUZZ_TEST(const uint8_t *data, size_t size) {
-  // FuzzedDataProvider provides convenience methods that turn the raw fuzzer
-  // data into common types. Use it to generate parameters for the function you
-  // want to fuzz:
-  //
-  // FuzzedDataProvider fuzzed_data(data, size);
-  // int my_int = fuzzed_data.ConsumeIntegral<int8_t>();
-  // std::string my_string = fuzzed_data.ConsumeRandomLengthString();
+  FuzzedDataProvider fuzzed_data(data, size); 
+  float num_1 = fuzzed_data.ConsumeFloatingPoint<float>();
+  float num_2 = fuzzed_data.ConsumeFloatingPoint<float>();
+  std::string op = fuzzed_data.ConsumeBytesAsString(1);
 
-  // Call the functions you want to test with the provided data and optionally
-  // assert that the results are as expected:
-  //
-  // int res = DoSomething(my_int, my_string);
-  // assert(res != -1);
+  char char_op = op[0];
 
-  // If you want to know more about writing fuzz tests you can check out the
-  // example projects at https://github.com/CodeIntelligenceTesting/cifuzz/tree/main/examples
-  // or have a look at our docs at https://docs.code-intelligence.com/
+  calculator(num_1, char_op, num_2);
 }
