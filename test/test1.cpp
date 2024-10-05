2d934ea3b4f53ccd203e039dcd20b511d8d477c0
juuso-Arch <juuso.sarkela@outlook.com>
2024-10-05 14:49:22 +0300

    Missing parenthesis

--- test/test1.cpp
+++ test/test1.cpp
@@ -14,7 +14,7 @@ FUZZ_TEST(const uint8_t *data, size_t size) {
   FuzzedDataProvider fuzzed_data(data, size); 
   float num_1 = fuzzed_data.ConsumeFloatingPoint<float>();
   float num_2 = fuzzed_data.ConsumeFloatingPoint<float>();
-  char op = fuzzed_data.PickValueInArray('+', '-', '*', '/');
+  char op = fuzzed_data.PickValueInArray({'+', '-', '*', '/'});
 
   calculator(num_1, op, num_2);
 }
